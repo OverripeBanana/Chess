675654efee0d03ddf8ee4161b21e788054bc9190
OverripeBanana <104330158+OverripeBanana@users.noreply.github.com>
2024-06-13 11:56:39 -0700

    added an input, made occupied squares illegal,  kinda

--- scripts/square.gd
+++ scripts/square.gd
@@ -5,28 +5,37 @@ extends Area2D
 @onready var circle = $Circle
 enum SquareColor {BLACK, WHITE}
 var myColor : SquareColor
+var originalColor
 #variables
 
 func _ready():
-	if rect.color == Color(0, 0, 0):
-		myColor = SquareColor.BLACK
-	else:
-		myColor = SquareColor.WHITE
+	originalColor = rect.color
 	
 func _physics_process(_delta):
 	var overlapping_areas = get_overlapping_areas()
 	if len(overlapping_areas) > 0:
 		for value in overlapping_areas:
-			if value.collision_layer == 2:
+			if value.collision_layer == 8:
 				illegal()
+				occupied()
 				break
 			else:
 				legal()
+				unoccupied()
 	else:
 		illegal()
+	if collision_layer == 5:
+		rect.color = Color(1, 0, 0, 1)	
+	else:
+		rect.color = originalColor
 		
 func legal():
 	circle.show()
 func illegal():
 	circle.hide()
+func occupied():
+	set_collision_layer_value(3, true)
 
+func unoccupied():
+	set_collision_layer_value(3, false)
+	
