b3baae271c9dd5a2efd1bffd6e3a96501e35a759
OverripeBanana <104330158+OverripeBanana@users.noreply.github.com>
2024-06-13 11:59:49 -0700

    Revert "added an input, made occupied squares illegal,  kinda"
    This reverts commit 675654efee0d03ddf8ee4161b21e788054bc9190.

--- scripts/piece.gd
+++ scripts/piece.gd
@@ -4,10 +4,8 @@ class_name Piece
 
 enum color {WHITE, BLACK}
 var selected : bool = false
-var wasClicked : bool = false
 @onready var mousebox : Area2D = $Mousebox
 @onready var movement : Area2D = $Movement
-@onready var rest : Area2D = $Rest
 
 func _ready():
 	mousebox.input_event.connect(_on_mousebox_input_event)
@@ -18,18 +16,15 @@ func _ready():
 func _process(_delta):
 	if selected:
 		followMouse()
-	
+
 func _on_mousebox_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton:
 		if event.pressed:
 			selected = true
-			wasClicked = true
-			moving()
 			enableMovementBox()
 			freezeMovementBox()
-		elif !event.pressed and wasClicked:
+		elif !event.pressed:
 			selected = false
-			atRest()
 			disableMovementBox()
 			snapToClosestSquare()
 			unfreezeMovementBox()
@@ -39,9 +34,8 @@ func searchForClosestSquare():
 	if len(legal_squares) != 0:
 		var closest_square = legal_squares[0]
 		for value in legal_squares:
-			if value.collision_layer != 5:
-				if position.distance_squared_to(value.position) < position.distance_squared_to(closest_square.position):
-					closest_square = value
+			if position.distance_squared_to(value.position) < position.distance_squared_to(closest_square.position):
+				closest_square = value
 		return closest_square
 	
 func snapToClosestSquare():
@@ -62,11 +56,4 @@ func enableMovementBox():
 	
 func followMouse():
 	position = get_global_mouse_position()
-
-func atRest():
-	rest.set_collision_layer_value(4, true)
-
-	
-func moving():
-	rest.set_collision_layer_value(4, false)
 
