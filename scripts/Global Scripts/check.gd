6007d81bd5fbcea4db16ec0d00929e714066ee0b
OverripeBanana <104330158+OverripeBanana@users.noreply.github.com>
2024-06-19 15:43:09 -0700

    worked out a few more bugs...
    worked out a few more bugs

--- scripts/Global Scripts/check.gd
+++ scripts/Global Scripts/check.gd
@@ -12,29 +12,31 @@ func _ready():
 	TurnManager.whiteTurnFinished.connect(_on_white_turn_finished)
 
 func _on_black_turn_finished():
-	if whiteInCheck:
-		await get_tree().create_timer(0.2).timeout
-		for piece in whitePieces:
-			if piece.get_name() != "White King":
-				for square in piece.legal_squares:
-					if square in whiteCancelCheckSquares:
-						print("not checkmate")
-						break
-					elif square == piece.legal_squares.back() and square not in whiteCancelCheckSquares and piece == whitePieces.back():
-						print("can't block or capture")
+	await get_tree().create_timer(0.2).timeout
+	print(canWhiteBlockorCapture())
 				
 func _on_white_turn_finished():
+	await get_tree().create_timer(0.2).timeout
+	print(canBlackBlockOrCapture())
+	
+func canBlackBlockOrCapture():
 	if blackInCheck:
-		await get_tree().create_timer(0.2).timeout
 		for piece in blackPieces:
 			if piece.get_name() != "Black King":
 				for square in piece.legal_squares:
 					if square in blackCancelCheckSquares:
-						print("not checkmate")
-						break
+						return true
 					elif square == piece.legal_squares.back() and square not in blackCancelCheckSquares:
-						print("can't block or capture")	
+						return false
 
-
+func canWhiteBlockorCapture():
+	if whiteInCheck:
+		for piece in whitePieces:
+			if piece.get_name() != "White King":
+				for square in piece.legal_squares:
+					if square in whiteCancelCheckSquares:
+						return true
+					elif square == piece.legal_squares.back() and square not in whiteCancelCheckSquares and piece == whitePieces.back():
+						return false
 
 					
